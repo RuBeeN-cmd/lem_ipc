b4f3a5a27f2bdf141281b07ffbe17892d8d25fb2
Ruben Rollin <rollinruben@live.fr>
2026-03-20 15:06:15 +0100

    Remove Comments

--- scripts/run.sh
+++ scripts/run.sh
@@ -22,8 +22,6 @@ for ((i=0; i<${nb_player}; i++));
 do
     team_id=$(((i % ${nb_team}) + 1))
     printf "%-25s Team: [   %-4d]\n" "Lauching number |${i}|" "${team_id}"
-    # valgrind --log-file=./val_log_player${i}.log 
-    # valgrind --log-file=./val_log_player${i}.log ${PROGRAM} ${team_id} >> ${LOGS_DIR}player.log & # >/dev/null 2>&1 &
     ${PROGRAM} ${team_id} ${board_width} ${board_height} >> ${LOGS_DIR}player${i}.log 2>&1 & # >/dev/null 2>&1 &
     sleep 0.0001
 done
